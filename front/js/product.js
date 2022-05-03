da4257b2907d5e90aa2a46345ef1da557ab59e72
Fr Dchamps <98919481+fkdchamps@users.noreply.github.com>
2022-05-03 09:36:44 +0200

    stockage panier

--- front/js/product.js
+++ front/js/product.js
@@ -1,45 +1,139 @@
-const getUrl = document.location.href;
-const url = new URL(getUrl);
-const id = url.searchParams.get("id");
+/* récupération de l'identifiant du produit dans l' url*/
+
+const urlObj = new URL(document.location.href);//on transforme la référence d'url en Objet URL exploitable par attributs
+const id = urlObj.searchParams.get("id");//on isole l'attribut d'identification
 
 /* requete du produit */
-const productPromise = fetch("http://localhost:3000/api/products/" + id);
-  productPromise.then(function(res) {
+fetch("http://localhost:3000/api/products/" + id)
+  .then(function(res) {
     if (res.ok) {
-      const product = res.json();
-      return product;
-      
+      return res.json();
     }
   })
-  .then(function(value) {
-    let productId = value._id;
-    console.log(productId);
-    let price = value.price;
-    let imgsrc = value.imageUrl;
-    console.log(imgsrc);
-    let kanapName = value.name;
-    console.log(kanapName);
-    let imgAlt = value.altTxt + ", " + kanapName;
-    console.log(imgAlt);
-    let descript = value.description;
-    console.log(descript);
-    const eltDivImg = document.querySelector("section.item .item__img");
-    console.log(eltDivImg);
-    eltDivImg.innerHTML = "<img src=" + imgsrc + " alt=" + imgAlt + "></img>"
-    const eltTitle = document.getElementById("title");
-    eltTitle.innerHTML = kanapName;
-    const eltPrice = document.getElementById("price");
-    eltPrice.innerHTML = price;
-    const eltDescription = document.getElementById("description");
-    eltDescription.innerHTML = descript;
-    const colors = value.colors;
-    const options = document.getElementById("colors");
-    for (let color of colors) {
-      console.log(color);
-      let option = document.createElement("option");
-      option.value = color;
-      option.label = color;
-      options.appendChild(option);
-      
-    }
+    /* mise à disposition des détails du produit */
+    .then(function(value) {
+      /* affectations attributs*/
+      let productId = value._id;
+      let price = value.price;
+      let imgsrc = value.imageUrl;
+      let kanapName = value.name;
+      let imgAlt = value.altTxt + ", " + kanapName;
+      let descript = value.description;
+
+      /* implementer l'image du produit */
+      const eltDivImg = document.querySelector("section.item .item__img");
+      eltDivImg.innerHTML = "<img src=" + imgsrc + " alt=" + imgAlt + "></img>";
+
+      /* implementer le titre */
+      const eltTitle = document.getElementById("title");
+      eltTitle.innerHTML = kanapName;
+
+      /* implementer le prix */
+      const eltPrice = document.getElementById("price");
+      eltPrice.innerHTML = price;
+
+      /* description */
+      const eltDescription = document.getElementById("description");
+      eltDescription.innerHTML = descript;
+
+      /* couleurs */
+      const colors = value.colors;
+      const options = document.getElementById("colors");
+      for (let color of colors) {
+        let option = document.createElement("option");
+        option.value = color;
+        option.label = color;
+        options.appendChild(option);
+      }
+    })
+    
+  .catch(function(err) {//récupération d'erreur si échec de réponse de la promesse
+    console.log(err);
   })
+  
+/****************************** */
+/* envoi de l'article au panier */
+/****************************** */
+
+
+/* au clic sur bouton on récupère l'id, la quantité et la couleur en un objet produit*/
+  //la couleur ici : ligne 67 <select name="color-select" id="colors">
+  //la quantité ici : ligne 76 <input type="number" name="itemQuantity" min="1" max="100" value="0" id="quantity">
+  //l'id dans la constante id du début
+  //penser à https://openclassrooms.com/fr/courses/5543061-ecrivez-du-javascript-pour-le-web/5578181-recuperez-des-donnees-utilisateurs-avec-les-evenements#/id/r-5591885
+
+const buttonCart = document.getElementById("addToCart"); //récup de l'élt sur lequel écouter
+buttonCart.addEventListener('click', function(event) { // On écoute l'événement click, et au clic on exécute la fonction
+  /* event.stopPropagation(); *///facultatif ici
+  /* récup des propriétés */
+  let color = document.getElementById("colors").value;
+  let quantity = parseInt(document.getElementById("quantity").value);
+  //la const id est déjà au début du code
+  /* création panier-tableau et produit en cours */
+  let cartridge = [];
+  let product = {color: color, quantity: quantity, id: id};
+  /* vérif quantité dans les normes admissibles */
+  if (product.quantity >= 1 && product.quantity <= 100 && product.color != "") {
+    /* vérif si le panier n'existe pas pour le créer et mettre le premier produit */
+    if (!localStorage.getItem("cartridge")) {
+      cartridge = [product];
+      localStorage.setItem("cartridge", JSON.stringify(cartridge));
+    /* sinon ajout du produit au panier */
+    }else{
+      let cartridgeString = localStorage.getItem("cartridge");//le panier du localstorage est importé sous forme de chaine
+      cartridge = JSON.parse(cartridgeString);//puis transposé en objet json
+      /* test de présence de l'article dans le panier existant par comparaison de propriété produit avec chaque cellule de panier */
+      let already = false;//témoin unique de fin de boucle
+      for (let i in cartridge) {
+        console.log(i);
+        if (product.id === cartridge[i].id && product.color === cartridge[i].color) {//si produit déjà présent
+          cartridge[i].quantity += product.quantity;//pousser seulement sa propriété quantité
+          already = true;//témoigner de cette présence
+        }         
+      }
+      if (already == false) {//si pas déjà présent, pousser l'objet produit dans panier
+        cartridge.push(product);
+      }
+      /* localStorage.removeItem("cartridge"); */
+      localStorage.setItem("cartridge", JSON.stringify(cartridge));
+    }
+  }
+})
+
+/* (on vérifie si il existe le tableau panier dans le local storage, sinon on le crée), et on l'importe en le traduisant dans une  variable tableau: =destringifier :-) */
+
+/* on  vérifie si un objet au même identifiant que le produit choisi n'est pas déjà présent, si ok on ajoute l'objet produit en tant que nouvel index du tableau sinon rien */
+
+/* on restringifie le tableau panier et on le reexporte dans le localstorage */
+
+
+/* un exemple de code à transposer pour notre tableau d'objets */
+/* var membres = [];
+function ajouter() {
+  var nom = document.getElementById('nom').value;
+  var prenom = document.getElementById('prenom').value;
+  var ville = document.getElementById('ville').value;
+  var statut = document.getElementById('stat').value;
+  var mail = document.getElementById('mail').value;
+  if (nom === '' || prenom === '')
+    alert('Nom et prenom sont obligatoires');
+  else {
+    membres = JSON.parse(localStorage.getItem('membres'));
+    membres.push({
+      Nom: nom,
+      Prenom: prenom,
+      DateDeNaissance: '11/01/1911',
+      Genre: 'Femme',
+      Statut: statut,
+      VillePays: ville,
+      Courriel: mail
+    });
+    localStorage.setItem('membres', JSON.stringify(membres));
+    console.log(membres);
+  }
+} */
+/* let article = "article" + id;
+let param = {id, quantity, color};
+if localStorage.getItem(article) = undefined
+  localStorage.setItem(article, JSON.stringify(param));
+ */
--- front/js/script.js
+++ front/js/script.js
@@ -1,44 +1,43 @@
-/* requete produits */
-const productsPromise = fetch("http://localhost:3000/api/products");
-  productsPromise.then(function(res) {
-    if (res.ok) {
-      const products = res.json();
-      return products;
-      
+/* requete produits et récupération au format json*/
+fetch("http://localhost:3000/api/products")//requete de promesse
+  .then(function(res) {//.then va enchainer la tache de fonction anonyme au retour la promesse, avec paramètre=reponse(évite de se faire doubler par l'asynchronisme)
+    if (res.ok) {//vérifie true si succès de réponse
+      return res.json();//retourne la méthode json de cette réponse*********?
     }
+  }).then(function(ArrayProduitsJson) {/* récupération et affichage des produits */ //deuxième enchainement de fonction pour extraire les données propres à chaque produit
+      let i=0//translation de l'index des objets du tableau transposé à partir de 1
+      for (let product of ArrayProduitsJson) {
+        i++;//on incrémente l'index
+
+        /* on affecte chaque donnée à une variable au nom parlant (étape à sauter avec l'expérience) */  
+        let productId = product._id;
+        let articleIndex = i;
+        let imgsrc = product.imageUrl;
+        let kanapName = product.name;
+        let imgAlt = "" + product.altTxt + ", " + kanapName;
+        let descript = product.description;
+
+        /* création de la balise <a> */
+        let newAnchor = document.createElement("a");//on crée la balise
+        newAnchor.href = "./product.html?id=" + productId;//on attribue l'url de référence du lien
+        newAnchor.id = "anchor" + articleIndex;//on affecte un id# unique à cette balise
+
+        /* affectation de la balise <a> dans le html */
+        const parentSection = document.getElementById("items");//on va chercher le bloc parent
+        parentSection.appendChild(newAnchor);//on injecte la balise html
+
+        /* creation, identification#, implementation, affectation dans son parent<a>, de l'article du produit */
+        let newArticle = document.createElement("article");
+        newArticle.id = "article" + articleIndex;
+        newArticle.innerHTML = "<img src=" + imgsrc + " alt=" + imgAlt + ">" +
+        "<h3 class='productName'>" + kanapName + "</h3>" +
+        "<p class='productDescription'>" + descript + "</p>";
+        newAnchor.appendChild(newArticle);
+        /* on a maintenant un nouveau produit affiché*/
+      }
+      /* on a maintenant en principe tous les produits affichés */
+    })
+  .catch(function(err) {//récupération d'erreur si échec de réponse de la promesse
+    console.log("Une erreur est survenue");
   })
-  .then(function(value) {
-    let i=0
-    for (let product of value) {
-      console.log(product);
-      i++;
-      console.log(i);      
-      let productId = product._id;
-      console.log(productId);
-      let articleId = "article" + i;
-      console.log(articleId);
-      let imgsrc = product.imageUrl;
-      console.log(imgsrc);
-      let kanapName = product.name;
-      console.log(kanapName);
-      let imgAlt = "" + product.altTxt + ", " + kanapName;
-      console.log(imgAlt);
-      let descript = product.description;
-      console.log(descript);
-      let newAnchor = document.createElement("a");
-      newAnchor.href = "./product.html?id=" + productId;
-      newAnchor.id = "anchor" + i;
-      const parentSection = document.getElementById("items");
-      parentSection.appendChild(newAnchor);
-      let newArticle = document.createElement("article");
-      newArticle.id = "article" + i;
-      newArticle.innerHTML = "<img src=" + imgsrc + " alt=" + imgAlt + ">" +
-      "<h3 class='productName'>" + kanapName + "</h3>" +
-      "<p class='productDescription'>" + descript + "</p>";
-      newAnchor.appendChild(newArticle);
-    }
-  })
-  .catch(function(err) {
-    console.log("Une erreur est survenue")
-  });
   
